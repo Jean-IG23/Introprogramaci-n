0ef7d18b558d0657d17188c54a8dabb33027f726
Jean-IG23 <jeaniiguez2004@gmail.com>
2022-10-21 22:47:33 -0500

    Creacion de nueva clase llamada virtualización

--- src/virtualización.java
+++ src/virtualización.java
@@ -2,6 +2,8 @@ public class virtualización {
     public static void main(String[] args) {
         System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVENCONES");
         System.out.println("realidad virtual");
+        System.out.println("creación de filtros");
+        System.out.println("creacion de dibujos en 3d");
         System.out.println("");
     }
 }
