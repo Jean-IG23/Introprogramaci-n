e154ac8f2a11ce1c1fd6892da0d2b50dda410d58
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2022-11-16 21:36:19 -0500

    Add files via upload

---
+++ src/GIT.java
@@ -0,0 +1,28 @@
+public class GIT {
+    public static void main(String[] args) {
+        System.out.println("NOMBRE Jean Carlos Iñiguez Gutierrez");
+        System.out.println("ASIGNATURA: Introducción a la Programación");
+        System.out.println("COMANDOS UTILIZADOS PARA LA CREACIÓN DEL ARCHIVO EN GIT");
+        System.out.println("Al principo voy a poner solo comandos que utilize para la creación del archivo y mas abajo explicare para que sirve cada uno de los comandos");
+        System.out.println("utilize el git init\n" +
+                "-git branch\n" +
+                "-git config --global user.email\n" +
+                "-git config --global user.name\n" +
+                "-git status\n" +
+                "-git commit\n" +
+                "-git add .\n" +
+                "-git remote add origin\n" +
+                "-git push");
+        System.out.println("ºahora explicare para que sirve cada uno de los comandos");
+        System.out.println("utilize el comandos de Git init para que se cree un nuevo repositorio local\n" +
+                "-El Git branh tanbien lo utilize para poder crear una rama de la cual se hiba a trabajar\n" +
+                "-El comando de Git config lo utilize para que el computador reconosca que email y que usuario va a trabajar en esta rama\n" +
+                "-El comando de Git status muestra la lista de los archivos que se han visto cambios o archivos que no estan cargados o confirmados\n" +
+                "-con Git commit lo que hace es guardar los cambios directo en el directorio de git\n" +
+                "-Git add este comando sirve para agregar archivos al area donde s estan preparando para ser subidos a git\n" +
+                "-con Git remote add origin lo utilize para conectar un repositorio local a un servidor remoto\n" +
+                "-Git push lo utilize para enviar confirmaciones locales hasta la rama maestra del repositorio remoto");
+        System.out.println(" Fueron los unicos comandos que use y hay esta la explicación de para que use cada uno de ellos ");
+
+    }
+}
