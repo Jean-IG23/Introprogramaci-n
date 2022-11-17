ef2667be76016ba0af4989469ee227fc384daeab
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2022-11-16 21:43:32 -0500

    Add files via upload

---
+++ src/contenido.java
@@ -0,0 +1,48 @@
+import java.sql.SQLOutput;
+
+public class contenido {
+    public static void main(String[] args) {
+        System.out.println("NOMBRE: Jean Carlos Iñiguez Gutierrez");
+        System.out.println("ASIGNATURA: Fundamentos Computacionales");
+        System.out.println("20 CONCEPTOS BÁSICOS DE LA PROGRAMACIÓN");
+        System.out.println("º un concepto básico es el lenguaje de la programación que esto es un conjunto de instrucciones que nos permiten dar intrucciones al computador");
+        System.out.println("º otro concepto de la programación es que hay varios tipos de lenguaje que son el:");
+        System.out.println("º Lenguaje de máquina Son aquellos cuyas instrucciones son\n" +
+                "directamente entendibles por la computadora y no necesitan\n" +
+                "traducción posterior para que la CPU pueda\n" +
+                "comprender y ejecutar el programa");
+        System.out.println("º Lenguaje de bajo nivel En este lenguaje las\n" +
+                "instrucciones se escriben en códigos alfabéticos\n" +
+                "conocidos como mnemotécnicos para las operaciones y\n" +
+                "direcciones simbólicas.");
+        System.out.println("º Lenguaje de alto nivel  son aquellos en los que las instrucciones o\n" +
+                "sentencias a la computadora son escritas con palabras similares\n" +
+                "a los lenguajes humanos, lo que facilita\n" +
+                "la escritura y\n" +
+                "comprensión del programa.");
+        System.out.println("º otra cosa basica de la programación son los algoritmos:");
+        System.out.println("º el algoritmo en si es un conjunto de operaciones que se utilizan para realizar diferentes acciones");
+        System.out.println("º hay varias face para la programación que lo resumire en los siguientes conceptos");
+        System.out.println("º hay varias faces la primera es el analisis del problema");
+        System.out.println("º El segundo paso es el diseño del algoritmo");
+        System.out.println("º el otro paso es la codificación que es la operación de escribir la solución del problema");
+        System.out.println("º el otro paso es el de la prueba consiste en la captura de datos hasta que\n" +
+                "el programa no presente errores");
+        System.out.println("º Otra cosa que se debe tener en cuenta es que siempre se le hace mantenimiento al programa que se esta ejecutando o realizando");
+        System.out.println("º Algo que aun no hemos visto pero q es algo basico en la programación es la estructura de de control");
+        System.out.println("º Las estructuras de control permiten al programador poder modificar el flujo de ejecución de un programa\n" +
+                "obligando a este a realizar repetidamente una serie de acciones o seleccionar para su ejecución una entre varias de ellas");
+        System.out.println("º Hay varios tipos de datos por ejemplo\n" +
+                "-Simples\n" +
+                "-Compuestos\n" +
+                "-Abstractos\n" +
+                "-Puntero\n" +
+                "-Objeto\n" +
+                "-etc");
+        System.out.println("º el pseudocodigo");
+        System.out.println("º el pseudocodigo es una forma de expresar los distintos pasos que se va a realizar un programa");
+        System.out.println("º lo siguiente es el diagrama de flujo");
+        System.out.println("º este diagrama sirve para describir un proceso, sistema o algoritmo informático. Esto se usa en diferentes campos por ejemplo estudiar planificar, mejorar y comunicar procesos");
+
+    }
+}
