1a41b14947521ace0d1d9f7bb0d31580f956d639
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-07-12 17:16:55 -0500

    Update POO_DATA_BASE.java

--- src/GIT.java
+++ src/GIT.java
@@ -1,28 +1,52 @@
-public class GIT {
+public class POO_DATA_BASE {
     public static void main(String[] args) {
-        System.out.println("NOMBRE Jean Carlos Iñiguez Gutierrez");
-        System.out.println("ASIGNATURA: Introducción a la Programación");
-        System.out.println("COMANDOS UTILIZADOS PARA LA CREACIÓN DEL ARCHIVO EN GIT");
-        System.out.println("Al principo voy a poner solo comandos que utilize para la creación del archivo y mas abajo explicare para que sirve cada uno de los comandos");
-        System.out.println("utilize el git init\n" +
-                "-git branch\n" +
-                "-git config --global user.email\n" +
-                "-git config --global user.name\n" +
-                "-git status\n" +
-                "-git commit\n" +
-                "-git add .\n" +
-                "-git remote add origin\n" +
-                "-git push");
-        System.out.println("ºahora explicare para que sirve cada uno de los comandos");
-        System.out.println("utilize el comandos de Git init para que se cree un nuevo repositorio local\n" +
-                "-El Git branh tanbien lo utilize para poder crear una rama de la cual se hiba a trabajar\n" +
-                "-El comando de Git config lo utilize para que el computador reconosca que email y que usuario va a trabajar en esta rama\n" +
-                "-El comando de Git status muestra la lista de los archivos que se han visto cambios o archivos que no estan cargados o confirmados\n" +
-                "-con Git commit lo que hace es guardar los cambios directo en el directorio de git\n" +
-                "-Git add este comando sirve para agregar archivos al area donde s estan preparando para ser subidos a git\n" +
-                "-con Git remote add origin lo utilize para conectar un repositorio local a un servidor remoto\n" +
-                "-Git push lo utilize para enviar confirmaciones locales hasta la rama maestra del repositorio remoto");
-        System.out.println(" Fueron los unicos comandos que use y hay esta la explicación de para que use cada uno de ellos ");
-
+        public class View_Est_Consola {
+        public static void main(String[] args) {
+        ConexionDB_Est obj = new ConexionDB_Est();
+        ArrayList<Estudiante> lstEstudiantes = obj.getLstEstudiantes();
+        for (Estudiante est : lstEstudiantes) {
+            System.out.println(est);    
+        }
     }
 }
+
+
+package MODEL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+public class ConexionDB_Est {
+    public Connection concDB;
+    public ArrayList <Estudiante> lstEstudiantes;
+    public String msj;
+    public void setConcDB(String url) {
+        try{
+         this.concDB = DriverManager.getConnection(url);
+        }catch(SQLException sqlException){
+        this.msj = sqlException.getMessage();
+    }
+        
+    }
+
+    public ArrayList<Estudiante> getLstEstudiantes() {
+        try{
+            lstEstudiantes = new ArrayList<Estudiante>();
+            setConcDB("jdbc:sqlite:src/MODEL/DB_Est.db");
+            Statement statement = concDB.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from Estudiante");
+            while(resultSet.next())
+                lstEstudiantes.add(new Estudiante(resultSet.getString("nombreEst"),
+                resultSet.getInt("nota1"),
+                resultSet.getInt("nota2"),
+                resultSet.getInt("promedio"),
+                resultSet.getString("estado")));
+        }catch(SQLException sqlException){
+            this.msj = sqlException.getMessage();
+        }
+        return lstEstudiantes;
+    }
+}
+
