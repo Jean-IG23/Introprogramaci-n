d50517b82462f11bff67dfb2c4d5b80a9ea9c3b2
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-08-30 13:27:49 -0500

    Update proyect1.java

--- src/CICLO2/proyect1.java
+++ src/CICLO2/proyect1.java
@@ -224,3 +224,229 @@ class Ejecutor {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package Ensayo;
+import java.util.Formatter;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Empleado4 {
+    private String nombre;
+    private String apellidos;
+    private String cedula;
+    private String direccion;
+    private int antiguedad;
+    private String telefono;
+    private double salario;
+
+    public Empleado4 (String nombre, String apellidos, String cedula, String direccion, int antiguedad, String telefono, double salario) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.cedula = cedula;
+        this.direccion = direccion;
+        this.antiguedad = antiguedad;
+        this.telefono = telefono;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+   }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+ public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double calcularSalario() {
+        return salario;
+    }
+}
+
+class Secretario extends Empleado4 {
+    private String despacho;
+    private double costoHora;
+    private int horasTrabajadas;
+    private int horasExtras;
+
+    public Secretario(String nombre, String apellidos, String cedula, String direccion, int antiguedad, String telefono, double salario, String despacho, double costoHora, int horasTrabajadas, int horasExtras) {
+        super(nombre, apellidos, cedula, direccion, antiguedad, telefono, salario);
+        this.despacho = despacho;
+        this.costoHora = costoHora;
+        this.horasTrabajadas = horasTrabajadas;
+        this.horasExtras = horasExtras;
+    }
+
+    @Override
+    public double calcularSalario() {
+        double salarioBase = super.calcularSalario();
+        double salarioHoras = costoHora * horasTrabajadas;
+        double salarioExtras = costoHora * horasExtras * 1.5;
+        return salarioBase + salarioHoras + salarioExtras;
+    }
+}
+
+class Vendedor4 extends Empleado4 {
+    private String cocheMatricula;
+    private String cocheMarca;
+    private String cocheModelo;
+    private String telefonoMovil;
+    private String areaVenta;
+    public double ventasMensuales = 500000000;
+    private double comision;
+   public Vendedor4 (String nombre, String apellidos, String cedula, String direccion, int antiguedad, String telefono, double salario, String cocheMatricula, String cocheMarca, String cocheModelo, String telefonoMovil, String areaVenta, double comision) {
+        super(nombre, apellidos, cedula, direccion, antiguedad, telefono, salario);
+        this.cocheMatricula = cocheMatricula;
+        this.cocheMarca = cocheMarca;
+        this.cocheModelo = cocheModelo;
+        this.telefonoMovil = telefonoMovil;
+        this.areaVenta = areaVenta;
+        this.comision = comision;
+    }
+
+    @Override
+    public double calcularSalario() {
+        double salarioBase = super.calcularSalario();
+        return salarioBase + (comision * ventasMensuales); // Debes definir 'ventasMensuales'
+    }
+}
+
+class JefeZona1 extends Empleado4 {
+    private String despacho;
+    private Secretario secretario;
+    private ArrayList<Vendedor4> vendedores1;
+
+    public JefeZona1(String nombre, String apellidos, String cedula, String direccion, int antiguedad, String telefono, double salario, String despacho, Secretario secretario, ArrayList<Vendedor4> vendedores1) {
+       super(nombre, apellidos, cedula, direccion, antiguedad, telefono, salario);
+        this.despacho = despacho;
+        this.secretario = secretario;
+        this.vendedores1 = vendedores1;
+    }
+    @Override
+    public double calcularSalario() {
+        double ventasTotales = 0;
+        for (Vendedor4 vendedor : vendedores1) {
+            ventasTotales += vendedor.ventasMensuales; // Debes definir 'ventasMensuales'
+        }
+        double salarioBase = super.calcularSalario();
+        return salarioBase + (ventasTotales * 0.1);
+    }
+}
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Secretario secretario = new Secretario("Ana", "López", "123456789", "Calle A, Ciudad", 2, "1234567890", 1000.0, "Oficina 101", 10.0, 160, 10);
+
+        Vendedor4 vendedor5 = new Vendedor4("Juan", "García", "987654321", "Avenida B, Pueblo", 3, "9876543210", 1500.0, "ABC123", "Toyota", "Corolla", "555-12345", "Zona Norte", 0.05);
+
+        Secretario secretarioJefe = new Secretario("Laura", "Pérez", "543216789", "Calle C, Ciudad", 5, "5432167890", 1200.0, "Oficina 202", 12.0, 180, 15);
+      ArrayList<Vendedor4> vendedoresJefe = new ArrayList<>();
+        vendedoresJefe.add(vendedor5);
+
+        JefeZona1 jefeZona1 = new JefeZona1("Carlos", "Martínez", "789654321", "Avenida D, Pueblo", 7, "7896543210", 2500.0, "Oficina 303", secretarioJefe, vendedoresJefe);
+
+        double salarioSecretario = secretario.calcularSalario();
+        double salarioVendedor = vendedor5.calcularSalario();
+        double salarioJefeZona = jefeZona1.calcularSalario();
+
+        System.out.println("Salario del Secretario: $" + salarioSecretario);
+        System.out.println("Salario del Vendedor: $" + salarioVendedor);
+        System.out.println("Salario del Jefe de Zona: $" + salarioJefeZona);
+        try (FileWriter writer = new FileWriter("empleados.csv")) {
+            writer.write("Tipo,Nombre,Apellidos,Cedula,Direccion,Antiguedad,Telefono,Salario\n");
+
+            writeEmpleado(writer, "Secretario", secretario, salarioSecretario);
+            writeEmpleado(writer, "Vendedor", vendedor5, salarioVendedor);
+            writeEmpleado(writer, "Jefe de Zona", jefeZona1, salarioJefeZona);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+  private static void writeEmpleado(FileWriter writer, String tipo, Empleado4 empleado, double salario) throws IOException {
+        writer.write(String.format("%s,%s,%s,%s,%s,%d,%s,%.2f\n",
+                tipo,
+                empleado.getNombre(),
+                empleado.getApellidos(),
+                empleado.getCedula(),
+                empleado.getDireccion(),
+                empleado.getAntiguedad(),
+                empleado.getTelefono(),
+                salario
+        ));
+    }
+}
